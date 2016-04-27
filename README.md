10c2f04639221f852378d9741d0c3184b8eb0d92
Josef Šimánek <josef.simanek@gmail.com>
2016-04-27 17:39:43 +0200

    Init

---
+++ README.md
@@ -0,0 +1,5 @@
+# PoC hrej.cz anti-adblock blocker (chrome extension)
+
+## How to install?
+
+Please don't install.
---
+++ blocker.js
@@ -0,0 +1,3 @@
+var script = document.createElement('script');
+script.textContent = 'window.bmone2n = bmone2n = "";';
+(document.head||document.documentElement).appendChild(script);
